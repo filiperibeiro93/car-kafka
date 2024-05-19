c38c3561b500a09126a61a9d55f33553a9dccfb9
Filipe Ribeiro <facribeiro.93@gmail.com>
2024-05-19 16:43:16 -0300

    kafka consumer

---
+++ src/main/java/com/store/car/message/KafkaConsumerMessage.java
@@ -0,0 +1,27 @@
+package com.store.car.message;
+
+import com.store.car.dto.CarPostDto;
+import com.store.car.service.CarPostService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class KafkaConsumerMessage {
+
+    private final CarPostService carPostService;
+
+    @Autowired
+    public KafkaConsumerMessage(CarPostService carPostService) {
+        this.carPostService = carPostService;
+    }
+
+    @KafkaListener(topics = "car-post-topic", groupId = "store-posts-group")
+    public void listening(CarPostDto dto) {
+        log.info("Received Car Post Information: {}", dto);
+        carPostService.newPostDetails(dto);
+    }
+
+}
