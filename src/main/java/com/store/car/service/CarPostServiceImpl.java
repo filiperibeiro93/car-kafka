462b704d7adae846cd047786cb2d3e94f22fdf1d
Filipe Ribeiro <facribeiro.93@gmail.com>
2024-05-19 16:07:31 -0300

    alterando tipo do price e listando sales

--- src/main/java/com/store/car/service/CarPostServiceImpl.java
+++ src/main/java/com/store/car/service/CarPostServiceImpl.java
@@ -1,12 +1,24 @@
 package com.store.car.service;
 
 import com.store.car.dto.CarPostDto;
+import com.store.car.repository.CarPostRepository;
+import com.store.car.repository.OwnerPostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CarPostServiceImpl implements CarPostService {
+
+    private final CarPostRepository carPostRepository;
+    private final OwnerPostRepository ownerPostRepository;
+
+    @Autowired
+    public CarPostServiceImpl(CarPostRepository carPostRepository, OwnerPostRepository ownerPostRepository) {
+        this.carPostRepository = carPostRepository;
+        this.ownerPostRepository = ownerPostRepository;
+    }
 
     @Override
     public void newPostDetails(CarPostDto dto) {
@@ -15,7 +27,9 @@ public class CarPostServiceImpl implements CarPostService {
 
     @Override
     public List<CarPostDto> getCarSales() {
-        return null;
+        return carPostRepository.findAll().stream()
+                .map(CarPostDto::fromModel)
+                .toList();
     }
 
     @Override
