d89b667234bed27a451c3c09f365351217a41517
Filipe Ribeiro <facribeiro.93@gmail.com>
2024-05-19 16:55:08 -0300

    criando car post no banco

--- src/main/java/com/store/car/service/CarPostServiceImpl.java
+++ src/main/java/com/store/car/service/CarPostServiceImpl.java
@@ -1,12 +1,14 @@
 package com.store.car.service;
 
 import com.store.car.dto.CarPostDto;
+import com.store.car.entity.CarPostEntity;
 import com.store.car.repository.CarPostRepository;
 import com.store.car.repository.OwnerPostRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -23,7 +25,15 @@ public class CarPostServiceImpl implements CarPostService {
 
     @Override
     public void newPostDetails(CarPostDto dto) {
+        var entity = new CarPostEntity();
+        BeanUtils.copyProperties(dto, entity);
 
+        var owner = ownerPostRepository.findById(dto.getOwnerId()).orElseThrow();
+        entity.setOwnerPost(owner);
+        entity.setContact(owner.getContactNumber());
+        entity.setCreatedAt(String.valueOf(new Date()));
+
+        carPostRepository.save(entity);
     }
 
     @Override
